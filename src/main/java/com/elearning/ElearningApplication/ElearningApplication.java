3cd973f70af054debd6ae8f40b5f7154eb1ed3d6
FerozKhanPatan <ferozsabr3@gmail.com>
2026-05-15 21:32:45 +0530

    Add enableJpaRepository

--- src/main/java/com/elearning/ElearningApplication/ElearningApplication.java
+++ src/main/java/com/elearning/ElearningApplication/ElearningApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @ComponentScan(basePackages = "com.elearning")
+@EnableJpaRepositories(basePackages = "com.elearning.repository")
 public class ElearningApplication {
 
     public static void main(String[] args) {
