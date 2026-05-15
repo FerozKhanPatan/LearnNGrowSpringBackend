b4d89c2b89c785f75d232dbfe425270aba6a4a21
FerozKhanPatan <ferozsabr3@gmail.com>
2026-05-15 21:37:48 +0530

    Add EntityScan to fix JPA entity scanning

--- src/main/java/com/elearning/ElearningApplication/ElearningApplication.java
+++ src/main/java/com/elearning/ElearningApplication/ElearningApplication.java
@@ -3,12 +3,14 @@ package com.elearning.ElearningApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @ComponentScan(basePackages = "com.elearning")
 @EnableJpaRepositories(basePackages = "com.elearning.repository")
+@EntityScan(basePackages = "com.elearning.model")
 public class ElearningApplication {
 
     public static void main(String[] args) {
